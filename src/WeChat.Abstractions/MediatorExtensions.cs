e2c7b72943a0ae01a97ef6f51f6381cdcea2493d
tooclian <tooclian@163.com>
2021-03-12 13:32:51 +0800

    add MediatorExtensions

---
+++ src/WeChat.Abstractions/MediatorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Mediator;
+
+using System.Threading.Tasks;
+
+namespace WeChat
+{
+    public static class MediatorExtensions
+    {
+        public static Task<TWeChatResponse> Send<TWeChatResponse>(this IMediator mediator,WeChatHttpRequestBase<TWeChatResponse> request,IWeChatSettings settings)
+            where TWeChatResponse : WeChatResponseBase
+        {
+            request.Configure(settings);
+            return mediator.Send(request);
+        }
+    }
+}
