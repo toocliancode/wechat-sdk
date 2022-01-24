3b1384d5c2563a00d8468d516774fef2c048a2ba
tooclian <tooclian@qq.com>
2022-01-24 16:16:02 +0800

    bug fix

--- src/WeChat.Abstractions/MediatorExtensions.cs
+++ src/WeChat.Abstractions/MediatorExtensions.cs
@@ -5,12 +5,13 @@ namespace WeChat;
 public static class MediationExtensions
 {
     public static Task<TWeChatResponse> Send<TWeChatResponse>(
-        this IMediator mediator,
+        this ISender mediator,
         WeChatHttpRequest<TWeChatResponse> request,
-        WeChatOptions options)
+        WeChatOptions options,
+        CancellationToken cancellationToken = default)
         where TWeChatResponse : WeChatHttpResponseBase, new()
     {
         request.WithOptions(options);
-        return mediator.Send(request);
+        return mediator.Send(request, cancellationToken);
     }
 }
