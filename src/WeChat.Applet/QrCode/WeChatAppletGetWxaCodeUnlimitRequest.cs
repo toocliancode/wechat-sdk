e188dba9226a068938cc42b42e50e73044a59ee5
tooclian <tooclian@qq.com>
2022-06-21 15:33:27 +0800

    fix bug

--- src/WeChat.Applet/QrCode/WeChatAppletGetWxaCodeUnlimitRequest.cs
+++ src/WeChat.Applet/QrCode/WeChatAppletGetWxaCodeUnlimitRequest.cs
@@ -30,16 +30,14 @@ public class WeChatAppletGetWxaCodeUnlimitRequest
     /// <param name="scene">最大32个可见字符，
     /// 只支持数字，大小写英文以及部分特殊字符：!#$&amp;'()*+,/:;=?@-._~，
     /// 其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）</param>
-    /// <param name="path">扫码进入的小程序页面路径，最大长度 128 字节，不能为空；
-    /// 对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"，
-    /// 即可在 wx.getLaunchOptionsSync 接口中的 query 参数获取到 {foo:"bar"}。</param>
+    /// <param name="page">页面 page，例如 pages/index/index，根路径前不要填加 /，不能携带参数（参数请放在 scene 字段里），如果不填写这个字段，默认跳主页面</param>
     /// <param name="width">二维码的宽度，单位 px。最小 280px，最大 1280px</param>
     /// <param name="autoColor">自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调</param>
     /// <param name="lineColor">auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示</param>
     /// <param name="isHyaline">是否需要透明底色，为 true 时，生成透明底色的小程序码</param>
     public WeChatAppletGetWxaCodeUnlimitRequest(
         string scene,
-        string path,
+        string page,
         int width = 430,
         bool autoColor = false,
         QrCodeLineColor? lineColor = null,
@@ -49,7 +47,7 @@ public class WeChatAppletGetWxaCodeUnlimitRequest
     {
         AccessToken = accessToken;
         Scene = scene;
-        Path = path;
+        Page = page;
         Width = width;
         AutoColor = autoColor;
         LineColor = lineColor;
@@ -73,8 +71,8 @@ public class WeChatAppletGetWxaCodeUnlimitRequest
     /// 对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"，
     /// 即可在 wx.getLaunchOptionsSync 接口中的 query 参数获取到 {foo:"bar"}。
     /// </summary>
-    [JsonPropertyName("path")]
-    public string Path { get; set; }
+    [JsonPropertyName("page")]
+    public string Page { get; set; }
 
     /// <summary>
     /// 二维码的宽度，单位 px。最小 280px，最大 1280px
