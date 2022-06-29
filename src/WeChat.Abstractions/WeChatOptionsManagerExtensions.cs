8821925b1dd44ad9dfb540381e1b5141bcb0dc44
tooclian <tooclian@qq.com>
2022-06-29 10:37:58 +0800

    添加配置管理接口

---
+++ src/WeChat.Abstractions/WeChatOptionsManagerExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace WeChat;
+
+public static class WeChatOptionsManagerExtensions
+{
+    public static Task<TOptions> GetDefaultAsync<TOptions>(this IWeChatOptionsManager manager) where TOptions : WeChatOptions, new()
+    {
+        return manager.GetAsync<TOptions>();
+    }
+}
